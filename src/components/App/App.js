d031f9335fe0fc6dfbf1f32873c956061461342c
bekzodnj <bekzodjon.norkuziev@gmail.com>
2019-09-14 23:19:33 +0200

    Added files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,25 +8,35 @@ import ItemStatusFilter from "../ItemStatusFilter";
 
 import "./App.css";
 
-const App = () => {
-  const todoData = [
-    { label: "Drink Coffee", important: false, id: 1 },
-    { label: "Build React App", important: true, id: 2 },
-    { label: "Have a lunch", important: true, id: 3 }
-  ];
+class App extends React.Component{
+   state = {
+    todoData : [
+      { label: "Drink Coffee", important: false, id: 1 },
+      { label: "Build React App", important: true, id: 2 },
+      { label: "Have a lunch", important: true, id: 3 }
+    ],
+   }
 
-  return (
-    <div className="todo-app">
-      <AppHeader todo={1} done={3} />
+   deleteItem = (id) =>{
+     //console.log(id);
 
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+       
+   }
+
+   render(){
+    return (
+      <div className="todo-app">
+        <AppHeader todo={1} done={3} />
+  
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+  
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
       </div>
-
-      <TodoList todos={todoData} onDeleted={id => console.log("del", id)} />
-    </div>
-  );
+    );
+   }
 };
 
 export default App;
